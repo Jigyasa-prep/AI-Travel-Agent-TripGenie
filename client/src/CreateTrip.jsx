7578511b805721f6bc35b96392baba519135446c
Jigyasa-prep <dasjigyasa1437@gmail.com>
2026-06-22 18:03:07 +0530

    Added routing pages structure

---
+++ client/src/CreateTrip.jsx
@@ -0,0 +1,9 @@
+function CreateTrip() {
+  return (
+    <div>
+      <h1>Create Your Trip</h1>
+    </div>
+  );
+}
+
+export default CreateTrip;
---
+++ client/src/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div>
+      <h1>TripGenie AI ✈️</h1>
+      <p>Plan Smarter Trips with AI</p>
+    </div>
+  );
+}
+
+export default Home;
